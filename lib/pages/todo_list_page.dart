c41c5447e69c1fefd1ac1fed017140085d2557db
Iago Alexandre <iagoaap@outlook.com>
2022-07-13 07:14:38 -0300

    get text digited for user

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
+  TodoListPage({Key? key}) : super(key: key);
+
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              hintText: 'exemplo@exemplo.com',
-              errorText: null,
-              prefixText: 'R\$',
-              suffixText: 'cm'
-            ),
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              color: Colors.purple,
-            ),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                  ),
+                  onChanged: onChanged,
+                  onSubmitted: onSubmitted,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: login, 
+                    child: Text('Entrar')
+                  ),
+                )
+              ],
+            )
           ),
-        )
-      ),
-    );
+        ),
+      );
+    }
+
+    void login() {
+      String text = emailController.text;
+      print('$text');
+      emailController.clear();
+    }
+
+    void onChanged(String text) {
+      //print(text);
+    }
+
+    void onSubmitted(String text) {
+      print(text);
+    }
   }
-}
